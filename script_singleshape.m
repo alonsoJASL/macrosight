e59d9fef8b6f87a363c1e92daf6b2a1d190e0f7f
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2017-07-26 13:16:21 +0100

    Minor updates

--- script_singleshape.m
+++ script_singleshape.m
@@ -18,66 +18,162 @@ trackinfo = tablenet(tablenet.track==2,[5 11 13 14]);
 % delete frames from the analysis.
 trackinfo(1:417,:)=[];
 trackMaxCorr = zeros(size(trackinfo,1),1);
-%% LOADING A SINGLE FRAME
+%% LOADING THE FIRST FRAME
 ix = 1;
 framet = trackinfo.timeframe(ix);
 
 thisseglabel = trackinfo.seglabel(ix);
 
-fprintf('\n%s: Loading original frame: %s.\n', ...
+fprintf('\n%s: Loading original (known) frame: %s.\n', ...
     mfilename, filenames{framet});
 
-[thisf] = getdatafromhandles(handles, filenames{framet});
+[knownfr] = getdatafromhandles(handles, filenames{framet});
 
-auxbinmat = thisf.clumphandles.nonOverlappingClumps==thisseglabel;
-thisf.regs = regionprops(auxbinmat, 'BoundingBox', 'Centroid', ...
+auxbinmat = knownfr.clumphandles.nonOverlappingClumps==thisseglabel;
+knownfr.regs = regionprops(auxbinmat, 'BoundingBox', 'Centroid', ...
     'EquivDiameter', 'MajorAxisLength', 'MinorAxisLength');
-thisf.boundy = bwboundaries(auxbinmat);
+knownfr.boundy = bwboundaries(auxbinmat);
 clear auxbinmat
 
-testImage = imfilter(thisf.dataGR, ...
-    imcrop(thisf.dataGR, thisf.regs.BoundingBox));
+testImage = imfilter(knownfr.dataGR,imcrop(knownfr.dataGR,...
+    knownfr.regs.BoundingBox));
 [trackMaxCorr(ix), mxidx] = max(testImage(:));
-[yinit, xinit] = ind2sub(size(thisf.dataGR), mxidx);
-thisf.xy = [yinit xinit];
+[yinit, xinit] = ind2sub(size(knownfr.dataGR), mxidx);
+knownfr.xy = [yinit xinit];
 
-thisf.test = testImage;
+knownfr.test = testImage;
 clear testImage mxidx yinit xinit;
 
-%% ONTO the following frames
-t0 = 5;
-frametplusT = framet + t0;
-[jumpf] = getdatafromhandles(handles, filenames{frametplusT});
-
-fprintf('\n%s: Compare frame: %s and Frame: %s.\n', ...
-    mfilename, filenames{framet}, filenames{frametplusT});
-
-%% 
+%% ALL THE REMAINING UNKNOWN FRAMES
 
 t0 = (ix+1):size(trackinfo,1);
 incr = 1;
 
+% set to false if you do not want images to be shown.
+debugvar = false;
 %jx=1;
+
+if debugvar==true
+    figure(111)
+    set(gcf, 'Position', get(0,'ScreenSize'));
+end
 for jx=1:incr:length(t0)
     frametplusT = trackinfo.timeframe(t0(jx));
     [auxstruct] = getdatafromhandles(handles, filenames{frametplusT});
+    auxstruct.seglabel = trackinfo.seglabel(t0(jx));
     
     testImage = imfilter(auxstruct.dataGR, ...
-        imcrop(thisf.dataGR, thisf.regs.BoundingBox));
+        imcrop(knownfr.dataGR, knownfr.regs.BoundingBox));
     
     [trackMaxCorr(jx), mxidx] = max(testImage(:));
-    [yinit, xinit] = ind2sub(size(thisf.dataGR), mxidx);
+    [yinit, xinit] = ind2sub(size(knownfr.dataGR), mxidx);
     
     auxstruct.xy = [yinit xinit];
-    auxstruct.test = testImage; 
+    auxstruct.test = testImage;
     
-    jumpf(jx) = auxstruct;
+    auxstruct.movedboundy = knownfr.boundy{1} + ...
+        repmat(auxstruct.xy-knownfr.xy, size(knownfr.boundy{1},1),1);
+    auxstruct.movedbb = knownfr.regs.BoundingBox + ...
+        [auxstruct.xy(2:-1:1) 0 0]-[knownfr.xy(2:-1:1) 0 0];
+    
+    % ukfr = u.k.fr = UnKnown FRame
+    ukfr(jx) = auxstruct;
+    
+    if debugvar == true
+        clf;
+        subplot(2,3,[1 2 4 5])
+        plotBoundariesAndPoints(ukfr(jx).dataGL, knownfr.boundy, ...
+            ukfr(jx).movedboundy, 'm-');
+        title(sprintf('Frame %s: original boundary vs moved boundary',...
+            filenames{frametplusT} ));
+        subplot(233)
+        plotBoundariesAndPoints(ukfr(jx).dataGL==ukfr(jx).seglabel,...
+            knownfr.boundy);
+        title('(not that it matters right now, but) check labels on jumpf');
+        subplot(236)
+        plotBoundariesAndPoints(ukfr(jx).test,...
+            knownfr.boundy, ukfr(jx).movedboundy, 'm-');
+        colormap parula;
+        pause;
+    end
 end
-clear testImage mxidx yinit xinit;
+clear testImage mxidx yinit xinit auxstruct;
+
+dist2known = distset2vect(vertcat(ukfr.xy),knownfr.xy,true);
+figure(1)
+clf
+plot(trackinfo.timeframe, trackMaxCorr, ...
+    trackinfo.timeframe, dist2known);
+hold on;
+plotHorzLine(trackinfo.timeframe, multithresh(dist2known,1));
+title('Cross correlation maximum value per unknown frame');
+xlabel(sprintf('Time frames: known (%s), unknown (%d and beyond)', ...
+    filenames{framet}, trackinfo.timeframe(t0(1))));
+legend('Cross correlation maximum value', ...
+    'Distance from the original position', ...
+    'Otsu of distances');
+grid on;
+xlim([trackinfo.timeframe(1) trackinfo.timeframe(end)]);
+
+figure(2)
+plotBoundariesAndPoints(knownfr.X, knownfr.boundy, vertcat(ukfr.xy), 'm*');
+
+%% PICK A SINGLE UNKNOWN FRAME
+
+t0 = (ix+1):size(trackinfo,1);
+jx=10;
+
+% set to false if you do not want images to be shown.
+debugvar = true;
+
+if debugvar==true
+    figure(111)
+    set(gcf, 'Position', get(0,'ScreenSize'));
+end
+
+frametplusT = trackinfo.timeframe(t0(jx));
+[auxstruct] = getdatafromhandles(handles, filenames{frametplusT});
+auxstruct.seglabel = trackinfo.seglabel(t0(jx));
+
+testImage = imfilter(auxstruct.dataGR, ...
+    imcrop(knownfr.dataGR, knownfr.regs.BoundingBox));
+
+[trackMaxCorr(jx), mxidx] = max(testImage(:));
+[yinit, xinit] = ind2sub(size(knownfr.dataGR), mxidx);
+
+auxstruct.xy = [yinit xinit];
+auxstruct.test = testImage;
+
+auxstruct.movedboundy = knownfr.boundy{1} + ...
+    repmat(auxstruct.xy-knownfr.xy, size(knownfr.boundy{1},1),1);
+auxstruct.movedbb = knownfr.regs.BoundingBox + ...
+    [auxstruct.xy(2:-1:1) 0 0]-[knownfr.xy(2:-1:1) 0 0];
+
+% ukfr = u.k.fr = UnKnown FRame
+ukfr(jx) = auxstruct;
+
+if debugvar == true
+    clf;
+    subplot(2,3,[1 2 4 5])
+    plotBoundariesAndPoints(ukfr(jx).dataGL, knownfr.boundy, ...
+        ukfr(jx).movedboundy, 'm-');
+    title(sprintf('Frame %s: original boundary vs moved boundary',...
+        filenames{frametplusT} ));
+    subplot(233)
+    plotBoundariesAndPoints(ukfr(jx).dataGL==ukfr(jx).seglabel,...
+        knownfr.boundy);
+    title('(not that it matters right now, but) check labels on jumpf');
+    subplot(236)
+    plotBoundariesAndPoints(ukfr(jx).test,...
+        knownfr.boundy, ukfr(jx).movedboundy, 'm-');
+    colormap parula;
+end
+
+clear testImage mxidx yinit xinit auxstruct;
 
 figure(1)
 plot(trackinfo.timeframe, trackMaxCorr);
-title('xcorr ');
+title('Maximum correlations ');
 
 figure(2)
-plotBoundariesAndPoints(thisf.X, thisf.boundy, vertcat(jumpf.xy), 'm*');
+plotBoundariesAndPoints(knownfr.X, knownfr.boundy, vertcat(ukfr.xy), 'm*');
