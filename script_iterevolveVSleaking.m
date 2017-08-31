a942e1740b63fc0f7b023c149f51c3037de52b6a
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2017-08-31 18:32:05 +0100

    CLUMP 8002-418:478 tests

--- script_iterevolveVSleaking.m
+++ script_iterevolveVSleaking.m
@@ -17,7 +17,6 @@ trackinfo = [tablenet(ismember(tablenet.track, clumplab),[5 1 2 11 13 14]) ...
 trackinfo(ismember(trackinfo.timeframe,DATASETHOLES),:) = [];
 
 %% Extract frames where the clump exists
-
 trackinfo(~ismember(trackinfo.timeframe, 418:478),:)=[];
 trackinfo = tablecompression(trackinfo, clumplab);
 %% FULL WORKFLOW (as in log)
@@ -30,110 +29,153 @@ framet = trackinfo.timeframe(tk);
 % 2. Compute the tracks' variables in the known frame
 [kftr] = getKnownTracksVariables(knownfr, trackinfo, clumplab, tk);
 
-% 2.1 Initialise a table with regionprops
-ttab = struct2table(kftr.regs(1));
-% 2.2 Unse anglegram measurements for LOLZ
-[ag] = computeMultiAnglegram(kftr.boundy(1));
+for ix=1:length(clumplab)
+    % 2.1 Initialise a table with regionprops
+    ttab{ix} = struct2table(kftr.regs(1));
+    % 2.2 Unse anglegram measurements for LOLZ
+    [ag] = computeMultiAnglegram(kftr.boundy(1));
+    
+    agopt.mainthresh = 150;
+    agopt.offsetVar = 7;
+    agopt.statsfname = 'max';
+    [candies, candyh] = computeCandidatePoints(ag, kftr.boundy{ix}, agopt);
+    
+    hag(ix).meanAg = candyh.meanAM;
+    hag(ix).stdAg = candyh.stdAM;
+    hag(ix).frobNorm = norm(ag,'fro');
+    %hag(ix).spectralRadius = max(abs(eig(ag'*ag)));
+    hag(ix).maxsvd = max(svd(ag));
+    hag(ix).rankAG = rank(ag);
+    hag(ix).rankRatio = (rank(ag)/size(ag,2));
+end
 
-agopt.mainthresh = 150;
-agopt.offsetVar = 7;
-agopt.statsfname = 'max';
-[candies, candyh] = computeCandidatePoints(ag, kftr.boundy{1}, agopt);
-
-hag.nCandy = size(candies(candyh.intensityPeaks>180,:),1);
-hag.meanAg = candyh.meanAM;
-hag.stdAg = candyh.stdAM;
-hag.frobNorm = norm(ag,'fro');
-hag.spectralRadius = max(abs(eig(ag'*ag)));
-hag.maxsvd = max(svd(ag));
-hag.rankAG = rank(ag);
-hag.rankRatio = hag.rankAG/size(ag,2);
 
 %% 3. start 'loop'
 % 3.1 Load the unknown frame
+debugvar = false;
+
 for tk=1:(length(trackinfo.timeframe)-1)
-tkp1 = tk+1;
-frametplusT = trackinfo.timeframe(tkp1);
-[ukfr] = getCommonVariablesPerFrame(handles, trackinfo, wuc, ...
-    filenames{frametplusT}, frametplusT);
-
-% 3.2 Evolve
-acopt.method = 'Chan-Vese';
-acopt.iter = 50;
-acopt.smoothf = 1.5;
-acopt.contractionbias = -0.1;
-acopt.erodenum = 5;
-[newfr] = nextframeevolution(ukfr, kftr, trackinfo, clumplab, acopt);
-
-% 3.3 Show preliminary results
-% LOOK AT LOGS
-if false
-figure(1)
-plotBoundariesAndPoints(ukfr.X, newfr.movedboundy, newfr.evoshape, 'm-');
-title(sprintf('Frame %d', frametplusT));
-if ukfr.hasclump == true
-    plotBoundariesAndPoints([],[],bwboundaries(ukfr.thisclump), ':y');
-end
-pause(0.3);
-end
-
-% 3.4 Update
-% 3.4.1 Update knownfr
-[knownfr, kftr] = updateKnownFrame(ukfr, newfr, clumplab);
-if ukfr.hasclump == true && false % change to true for updating info to disk
-    update2disk(handles, knownfr, newfr, wuc);
-end
-framet = frametplusT;
-
-ttab = [ttab;struct2table(kftr.regs(1))];
-% 2.2 Unse anglegram measurements for LOLZ
-[ag] = computeMultiAnglegram(kftr.boundy(1));
-[candies, candyh] = computeCandidatePoints(ag, kftr.boundy{1}, agopt);
-
-hag.nCandy = [hag.nCandy;size(candies(candyh.intensityPeaks>180,:),1)];
-hag.meanAg = [hag.meanAg; candyh.meanAM];
-hag.stdAg = [hag.stdAg; candyh.stdAM];
-hag.frobNorm = [hag.frobNorm; norm(ag,'fro')];
-hag.spectralRadius = [ hag.spectralRadius; max(abs(eig(ag'*ag)))];
-hag.maxsvd = [hag.maxsvd; max(svd(ag))];
-hag.rankAG = [hag.rankAG; rank(ag)];
-hag.rankRatio =[hag.rankRatio; (rank(ag)/size(ag,2))];
+    tkp1 = tk+1;
+    frametplusT = trackinfo.timeframe(tkp1);
+    [ukfr] = getCommonVariablesPerFrame(handles, trackinfo, wuc, ...
+        filenames{frametplusT}, frametplusT);
+    
+    % 3.2 Evolve
+    acopt.method = 'Chan-Vese';
+    acopt.iter = 50;
+    acopt.smoothf = 1.5;
+    acopt.contractionbias = -0.1;
+    acopt.erodenum = 5;
+    [newfr] = nextframeevolution(ukfr, kftr, trackinfo, clumplab, acopt);
+    
+    % 3.4 Update
+    % 3.4.1 Update knownfr
+    [knownfr, kftr] = updateKnownFrame(ukfr, newfr, clumplab);
+    if ukfr.hasclump == true && false % change to true for updating info to disk
+        update2disk(handles, knownfr, newfr, wuc);
+    end
+    framet = frametplusT;
+    
+    anglegrams = cell(length(clumplab),1);
+    for ix=1:length(clumplab)
+        % 2.1 Initialise a table with regionprops
+        ttab{ix} = [ttab{ix};struct2table(kftr.regs(ix))];
+        % 2.2 Unse anglegram measurements for LOLZ
+        [ag] = computeMultiAnglegram(kftr.boundy(ix));
+        anglegrams{ix} = ag;
+        
+        agopt.mainthresh = 150;
+        agopt.offsetVar = 7;
+        agopt.statsfname = 'max';
+        [candies, candyh] = computeCandidatePoints(ag, kftr.boundy{ix}, agopt);
+        
+        hag(ix).meanAg = [hag(ix).meanAg; candyh.meanAM];
+        hag(ix).stdAg = [hag(ix).stdAg; candyh.stdAM];
+        hag(ix).frobNorm = [hag(ix).frobNorm; norm(ag,'fro')];
+        %hag(ix).spectralRadius = [hag(ix).spectralRadius; max(abs(eig(ag'*ag)))];
+        hag(ix).maxsvd = [hag(ix).maxsvd; max(svd(ag))];
+        hag(ix).rankAG = [hag(ix).rankAG; rank(ag)];
+        hag(ix).rankRatio =[hag(ix).rankRatio; (rank(ag)/size(ag,2))];
+    end
+    
+    % 3.3 Show preliminary results
+    % LOOK AT LOGS
+    if debugvar == true
+        f11=figure(11);
+        plotBoundariesAndPoints(ukfr.X, newfr.movedboundy, newfr.evoshape, 'm-');
+        title(sprintf('Frame %d', frametplusT));
+        if ukfr.hasclump == true
+            plotBoundariesAndPoints([],[],bwboundaries(ukfr.thisclump), ':y');
+        end
+        
+        f12=figure(12);
+        for qx=1:length(clumplab)
+            subplot(1,length(clumplab),qx)
+            anglegrams{qx}(end,end) = 340;
+            imagesc(anglegrams{qx});
+            colorbar;
+            set(f12, 'Position', ceil(get(0,'ScreenSize')./2));
+            anglegrams{qx}(end,end) = 0;
+        end
+        
+        if tk==1
+            f1 = getframe(f11);
+            [im, map] = rgb2ind(f1.cdata, 256, 'nodither');
+            im(1,1,1,(length(trackinfo.timeframe)-1)) = 0;
+            
+            f2 = getframe(f12);
+            [im2, map2] = rgb2ind(f2.cdata, 256, 'nodither');
+            im2(1,1,1,(length(trackinfo.timeframe)-1)) = 0;
+        else
+            f1 = getframe(f11);
+            f2 = getframe(f12);
+            %[im2, map2] = rgb2ind(f2.cdata, 256, 'nodither');
+        end
+        im(:,:,1,tk) = rgb2ind(f1.cdata, map, 'nodither');
+        im2(:,:,1,tk) = rgb2ind(f2.cdata, map2, 'nodither');
+        pause(0.3);
+    end
+    
 end
 %tk = tk+1;
-
+if debugvar == true
+    gifname = sprintf('clump%d-frames%dto%d.gif', ...
+        wuc, trackinfo.timeframe(1), trackinfo.timeframe(end));
+    imwrite(im,map,gifname ,'DelayTime', 0.75, 'LoopCount',inf);
+    imwrite(im2,map2,strcat('anglegrams-',gifname),...
+        'DelayTime', 0.75, 'LoopCount',inf);
+    clear gifname;
+end
 %% Shape measurements
+whichlab = 1;
+propt = ttab{whichlab};
+agtab = hag(whichlab);
 
-aspectRatio = ttab.MinorAxisLength./ttab.MajorAxisLength;
-circularity = (4*pi).*(ttab.Area./(ttab.Perimeter.^2));
+aspectRatio = propt.MinorAxisLength./propt.MajorAxisLength;
+circularity = (4*pi).*(propt.Area./(propt.Perimeter.^2));
 
-propertiesTable = [ttab(:,1) table(aspectRatio, circularity) ttab(:,5:end)];
-agramTable = struct2table(hag);
-agramTable(:,1) = [];
-%% 
+propertiesTable = [propt(:,1) table(aspectRatio, circularity) propt(:,5:end)];
+agramTable = struct2table(agtab);
+%
 ffns = fieldnames(propertiesTable);
+ffns2 = fieldnames(agramTable);
+
+ffns2(end-2:end) = [];
 ffns(end-2:end) = [];
 
-figure(1)
+figure(clumplab(whichlab))
 for ix=1:length(ffns)
-    subplot(2,3,ix)
+    subplot(4,3,ix)
     whichY = propertiesTable.(ffns{ix});
     plotVarandDiff(trackinfo.timeframe, whichY, ffns{ix});
     title(ffns{ix});
     grid on
-end
-
-
-ffns = fieldnames(agramTable);
-ffns(end-2:end) = [];
-
-figure(2)
-for ix=1:length(ffns)
-    subplot(2,4,ix)
-    whichY = agramTable.(ffns{ix});
-    plotVarandDiff(trackinfo.timeframe, whichY, ffns{ix});
-    title(ffns{ix});
+    
+    subplot(4,3,6+ix)
+    whichY = agramTable.(ffns2{ix});
+    plotVarandDiff(trackinfo.timeframe, whichY, ffns2{ix});
+    title(ffns2{ix});
     grid on
 end
-
 
 
