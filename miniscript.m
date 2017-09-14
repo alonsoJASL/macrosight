397a3e1f29cb2760a01ecddc1ef32a13fe251596
Jose Alonso Solis-Lemus <alonso.jasl@gmail.com>
2017-09-14 19:48:55 +0100

    single tracks test 1
    No change in AC options

--- miniscript.m
+++ miniscript.m
@@ -39,26 +39,3 @@ else
 end
 
 
-function [opt] = acoptions(actype)
-%
-switch actype
-    case 'shrink'
-        opt.method = 'Chan-Vese';
-        opt.iter = 100;
-        opt.smoothf = 1.25;
-        opt.contractionbias = 0.1;
-        opt.erodenum = 7;
-    case 'grow'
-        opt.method = 'Chan-Vese';
-        opt.iter = 200;
-        opt.smoothf = 1;
-        opt.contractionbias = -0.25;
-        opt.erodenum = 3;
-    case 'nothing'
-        opt.method = 'Chan-Vese';
-        opt.iter = 50;
-        opt.smoothf = 1.5;
-        opt.contractionbias = -0.1;
-        opt.erodenum = 5;
-end
-end
--- script_singleanalysis.m
+++ script_singleanalysis.m
@@ -29,10 +29,12 @@ flabs(idxshorttrax) = [];
 flabfreq(idxshorttrax) = [];
 cllength(idxshorttrax) = [];
 alltracks(idxshorttrax) = [];
+
 %% CHOOSE ONE TRACK AND SEPARATE INTO MANY SINGLE PATHS (TRACK >> PATH)
 clc;
 
-ix=33;
+%ix=1;
+for ix=1:length(flabs)
 wuc=flabs(ix);
 trackinfo=alltracks{ix};
 
@@ -55,6 +57,43 @@ for jx=1:size(wendys,1)
     allpaths{jx} = trackinfo(wendys(jx,1):wendys(jx,2),:);
 end
 
-% from here, just choose a path and check the evolutions
+outfolder = sprintf('%s_mat_TRACKS_lab%d',handlesdir.data, wuc);
+if ~isdir(fullfile(handlesdir.pathtodir, outfolder))
+    fprintf('%s: Creating folder %s.\n', mfilename, outfolder);
+    mkdir(fullfile(handlesdir.pathtodir, outfolder));
+end
+outtrackname = sprintf('label%dtracks-knwonfr', wuc);
 
-%% NOW TRY THE SAME WITH CLUMPS
+% from here, just choose a path and check the evolutions
+acopt=acoptions('nothing');
+for jx=1:length(allpaths)
+    fprintf('\n%s: Analysing path=%d/%d from TRACK=%d\n',...
+        mfilename, jx, length(allpaths), wuc);
+    thispath = allpaths{jx};
+    if size(thispath,1)>1
+        framet = thispath.timeframe(1);
+        [knownfr] = getCommonVariablesPerFrame(handles, thispath, wuc, ...
+            filenames{framet}, framet);
+        [kftr] = getKnownTracksVariables(knownfr, thispath, clumplab, 1);
+        
+        save(fullfile(handlesdir.pathtodir, outfolder, ...
+            sprintf('%s%d-acNOTHING.mat',outtrackname,framet)),...
+            'knownfr', 'kftr');
+        
+        for tk=1:(size(thispath,1)-1)
+            frametplusT = thispath.timeframe(tk+1);
+            [ukfr] = getCommonVariablesPerFrame(handles, thispath, wuc, ...
+                filenames{frametplusT}, frametplusT);
+            [newfr] = nextframeevolution(ukfr, kftr, trackinfo, clumplab, acopt);
+            %miniscript;
+            
+            [knownfr, kftr] = updateKnownFrame(ukfr, newfr, clumplab);
+            save(fullfile(handlesdir.pathtodir, outfolder, ...
+                sprintf('%s%d.mat',outtrackname,frametplusT)),...
+                'knownfr', 'kftr', 'newfr');
+        end
+    else
+        fprintf('%s: Track too short!')
+    end
+end
+end
