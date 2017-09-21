706e52de8920bf00229fdb0c439ffcbf5ac06405
Jose Alonso Solis Lemus <alonso.jasl@gmail.com>
2017-09-21 17:10:44 +0100

    Aux file untitled.m to generate full frames after single cell analysis

---
+++ untitled.m
@@ -0,0 +1,55 @@
+%% 
+
+initscript;
+
+load DATASETHOLES;
+clumptracktable(ismember(tablenet.timeframe, DATASETHOLES), :)=[];
+tablenet(ismember(tablenet.timeframe, DATASETHOLES),:)=[];
+
+rootdir = handlesdir.pathtodir;
+%% 
+
+a = dir(fullfile(handlesdir.pathtodir, [handlesdir.data '_mat_TRACKS_lab*']));
+a = {a.name};
+acnames = a(contains(a,'ACOPTIONS'));
+
+acoutdir = fullfile(rootdir, [handlesdir.data '_mat_ACOPTIONS_FULL']);
+
+if ~isdir(acoutdir)
+    mkdir(acoutdir); 
+end
+
+for ix = 1:3
+disp(acnames{ix});
+
+b = dir(fullfile(handlesdir.pathtodir, acnames{ix},'*.mat'));
+b = {b.name};
+
+for jx = 1:3
+    fprintf('\t%s\n', b{jx});
+c = load(fullfile(rootdir, acnames{ix}, b{jx}));
+
+if exist(fullfile(acoutdir, c.meta.fname), 'file')
+    load(fullfile(acoutdir, c.meta.fname));
+    
+    fullfr.initboundy(end+1) = c.frameinfo.initboundy;
+    fullfr.outboundy(end+1) = c.frameinfo.outboundy;
+    fullfr.newSegm = bitor(fullfr.newSegm, ...
+        poly2mask(c.frameinfo.outboundy{1}(:,2), ...
+        c.frameinfo.outboundy{1}(:,1), handles.rows, handles.cols));
+    fullfr.anglegram(:,:,end+1) = computeMultiAnglegram(fullfr.outboundy);
+else 
+    fullfr.X = c.frameinfo.X;
+    fullfr.dataL = c.frameinfo.dataL;
+    fullfr.dataGL = c.frameinfo.dataGL;
+    fullfr.initboundy = c.frameinfo.initboundy;
+    fullfr.outboundy = c.frameinfo.outboundy;
+    fullfr.newSegm = poly2mask(c.frameinfo.outboundy{1}(:,2), ...
+        c.frameinfo.outboundy{1}(:,1), handles.rows, handles.cols);
+    fullfr.anglegram = computeMultiAnglegram(fullfr.outboundy);
+end
+meta = c.meta;
+
+save(fullfile(acoutdir, c.meta.fname), 'fullfr', 'meta');
+end
+end
