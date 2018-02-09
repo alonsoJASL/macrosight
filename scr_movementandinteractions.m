530a8ef5afd16041986d3dcc6c845e2bd645c6dc
Jose Alonso (mac) <alonso.jasl@gmail.com>
2018-02-09 18:59:59 +0000

    Movement and interactions

--- scr_movementandinteractions.m
+++ scr_movementandinteractions.m
@@ -4,7 +4,7 @@
 %
 %% Initialisation
 whichmacro = 2;
-initscript; 
+initscript;
 
 %% Create trackinfo from clump frames
 % Choose the entries in `tablenet` that contain the tracks in
@@ -17,8 +17,45 @@ clumplab = getlabelsfromcode(wuc);
 trackinfo = [tablenet(ismember(tablenet.track, clumplab),[5 1 2 9 11 13 14]) ...
     clumptracktable(ismember(tablenet.track, clumplab),:)];
 
-disp(trackinfo);
+ffix = 6;%min(trackinfo.timeframe);
+lfix = 72;%max(trackinfo.timeframe);
 
+trackinfo(~ismember(trackinfo.timeframe, ffix:lfix),:) = [];
+
+%%
+if length(clumplab)>1
+    [trackinfo] = tablecompression(trackinfo, clumplab);
+end
+[allpaths, wendys] = getpathsperlabel(8007, trackinfo);
+wendys(:,2) = wendys(:,2)-1;
+
+for ix=1:(size(wendys,1)-1)
+    clumpwendys(ix,:) = [wendys(ix,2)+1 wendys(ix+1,1)-1 ix ix+1];
+    allclumppaths{ix} = trackinfo(clumpwendys(ix,1):clumpwendys(ix,2),:);
+end
+
+%%
+
+wcix = 3; % which clump interaction index.
+citab = allclumppaths{wcix};
+meanXY = [mean(allclumppaths{wcix}.X,1)' mean(allclumppaths{wcix}.Y,1)']; % [X Y]
+
+pretab = allpaths{clumpwendys(wcix,3)};
+posttab = allpaths{clumpwendys(wcix,4)};
+
+preline = [pretab([1 end],:).X pretab([1 end],:).Y];
+postline = [posttab([1 end],:).X posttab([1 end],:).Y];
+
+
+for jx=pretab.timeframe(1):posttab.timeframe(end)
+    %thisfr = getdatafromhandles(handles, filenames{trackinfo.timeframe(jx)});
+    thisfr = getdatafromhandles(handles, filenames{jx});
+    clf;
+    plotBoundariesAndPoints(thisfr.X, bwboundaries(thisfr.clumphandles.overlappingClumps>0), meanXY,'cm');
+    plot(preline(:,2), preline(:,1), '-xr');
+    plot(postline(:,2), postline(:,1), '-vg');
+    pause;
+end
 %% plot all the tracks by frames
 
 debugvar = true;
@@ -38,21 +75,21 @@ for ix=ffix:lfix%282:462%282:410%1:size(trackinfo, 1)
     halfposition;
     removeBorders;
     if debugvar
-    if ix==ffix
-        f = getframe(gcf);
-        [im, map] = rgb2ind(f.cdata, 256, 'nodither');
-        im(1,1,1,length(ffix:lfix)) = 0;
-    else
-        f = getframe(gcf);
-        im(:,:,1,ix) = rgb2ind(f.cdata, map, 'nodither');
-    end
+        if ix==ffix
+            f = getframe(gcf);
+            [im, map] = rgb2ind(f.cdata, 256, 'nodither');
+            im(1,1,1,length(ffix:lfix)) = 0;
+        else
+            f = getframe(gcf);
+            im(:,:,1,ix) = rgb2ind(f.cdata, map, 'nodither');
+        end
     end
     pause(0.1);
 end
 if debugvar
-imwrite(im,map,['./figs/visualisation-macros' num2str(whichmacro) ...
-    '-clump' num2str(wuc)  '.gif'],...
-    'DelayTime', 0.1, 'LoopCount',inf);
+    imwrite(im,map,['./figs/visualisation-macros' num2str(whichmacro) ...
+        '-clump' num2str(wuc)  '.gif'],...
+        'DelayTime', 0.1, 'LoopCount',inf);
 end
 
 %% plot each frame with the track
@@ -100,9 +137,9 @@ lastframeidx = find(trackinfo.timeframe==lfix);
 for ix=firstframeidx:lastframeidx%282:462%282:410%1:size(trackinfo, 1)
     thisfr = getdatafromhandles(handles, filenames{trackinfo.timeframe(ix)});
     %if ix==firstframeidx
-     %   plotframeandpoint(thisfr, trackinfo, ix);
+    %   plotframeandpoint(thisfr, trackinfo, ix);
     %else
-        plotframeandpoint([], trackinfo, ix);
+    plotframeandpoint([], trackinfo, ix);
     %end
     title(filenames{trackinfo.timeframe(ix)});
     pause(0.1);
